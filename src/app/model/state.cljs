28c74e0b47ae8b69b9666b51895b023853df36cf
Phil Jackson <phil@shellarchive.co.uk>
2020-06-09 00:00:05 +0100

    More tests and tighter specs.

--- src/app/model/state.cljs
+++ src/app/model/state.cljs
@@ -15,10 +15,12 @@
 
 (s/def ::uid string?)
 (s/def ::display-name string?)
-(s/def ::email string?)
+(s/def ::email (s/and string? #(re-matches #".+?@.+?\..+" %)))
 
-(s/def ::user (s/or :valid-user (s/keys :req-un [::uid ::display-name ::email])
-                    :no-user #(= % {})))
+(s/def ::user (s/or :not-initialised     nil?
+                    :valid-firebase-user (s/keys :req-un [::uid ::display-name ::email])
+                    :no-firebase-user    #(= % {})))
+
 (s/def ::state (s/keys :req-un [::config ::cv-data ::route-match]
                        :opt-un [::user]))
 
@@ -29,8 +31,8 @@
 (def match  (r/cursor all-seeing-state [:route-match]))
 (def view   (r/cursor all-seeing-state [:route-match :data :view]))
 
-#_(when debug?
-    (s/explain ::cv-data @cvs)
-    (s/explain ::config @config)
-    (s/explain ::user @user)
-    (s/explain ::route-match @match))
+(when debug? 
+  (s/explain ::cv-data @cvs)
+  (s/explain ::config @config)
+  (s/explain ::user @user)
+  (s/explain ::route-match @match))
--- src/test/model/state_test.cljs
+++ src/test/model/state_test.cljs
@@ -3,32 +3,38 @@
             [cljs.spec.alpha :as s]
             [cljs.test :include-macros true :refer [deftest is run-tests testing are]]))
 
-(deftest check-user-specs
-  (testing "::state/id valid values"
-    (are [value] (s/valid? ::state/id value)
-      "string"))
+(deftest check-specs
+  (testing "::state/id"
+    (testing "valid values"
+      (are [value] (s/valid? ::state/id value)
+        "1234567890"
+        "hello"))
 
-  (testing "::state/id invalid values"
-    (are [value] (not (s/valid? ::state/id value))
-      1
-      {:one 1}
-      :one
-      []
-      [1 2 3]))
+    (testing "invalid values"
+      (are [value] (not (s/valid? ::state/id value))
+        nil
+        #uuid "b56f61e8-312f-4f7b-8ce6-e0a49b93ed57"
+        1
+        {:one 1}
+        :one
+        []
+        [1 2 3])))
 
-  (testing "::state/user valid values"
-    (are [value] (s/valid? ::state/user value)
-      {}
-      {:uid "hi"
-       :email "an@email.com"
-       :display-name "Phil Jackson"}))
+  (testing "::state/user"
+    (testing "valid values"
+      (are [value] (s/valid? ::state/user value)
+        nil
+        {}
+        {:uid "hi"
+         :email "an@email.com"
+         :display-name "Phil Jackson"}))
 
-  (testing "::state/user invalid values"
-    (are [value] (not (s/valid? ::state/user value))
-      1
-      {:one 1}
-      :one
-      []
-      {:uid "hello"}
-      {:uid "hello" :email "some@email.com"}
-      [1 2 3])))
+    (testing "invalid values"
+      (are [value] (not (s/valid? ::state/user value))
+        1
+        {:one 1}
+        :one
+        []
+        {:uid "hello"}
+        {:uid "hello" :email "some@email.com"}
+        [1 2 3]))))
