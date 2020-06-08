4ec1b0ee59553a71b0b4a2ee7f43caab38932a85
Phil Jackson <phil@shellarchive.co.uk>
2020-06-09 00:22:51 +0100

    Add a script to start npm sessions.

---
+++ bin/tmux.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+tmux new-session -d -s CVC -n "App"
+tmux send-keys -t "=CVC:App" "npm start" Enter
+
+tmux new-window -d -t '=CVC' -n "Test"
+tmux send-keys -t "=CVC:Test" "npm test" Enter
+
+tmux new-window -d -t '=CVC' -n "Less"
+tmux send-keys -t "=CVC:Less" "npm run less" Enter
+
+[ -n "${TMUX:-}" ] &&
+    tmux switch-client -t '=CVC' ||
+    tmux attach-session -t '=CVC'
