edeec7f7876934dd34653a0369c868e71051ce67
Phil Jackson <phil@shellarchive.co.uk>
2020-06-10 23:04:17 +0100

    Use similar config for http servers.

--- bin/tmux.bash
+++ bin/tmux.bash
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 tmux new-session -d -s CVC -n "App"
 tmux send-keys -t "=CVC:App" "npm start" Enter
-
-tmux new-window -d -t '=CVC' -n "Test"
-tmux send-keys -t "=CVC:Test" "sleep 5 && npm test" Enter
 
 tmux new-window -d -t '=CVC' -n "Less"
 tmux send-keys -t "=CVC:Less" "npm run less" Enter
