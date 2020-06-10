6c3f65eaa91b129d6ea7a9470d7ca2f41b12aff1
Phil Jackson <phil@shellarchive.co.uk>
2020-06-10 22:43:55 +0100

    Change test port.

--- bin/tmux.bash
+++ bin/tmux.bash
@@ -4,7 +4,7 @@ tmux new-session -d -s CVC -n "App"
 tmux send-keys -t "=CVC:App" "npm start" Enter
 
 tmux new-window -d -t '=CVC' -n "Test"
-tmux send-keys -t "=CVC:Test" "npm test" Enter
+tmux send-keys -t "=CVC:Test" "sleep 5 && npm test" Enter
 
 tmux new-window -d -t '=CVC' -n "Less"
 tmux send-keys -t "=CVC:Less" "npm run less" Enter
