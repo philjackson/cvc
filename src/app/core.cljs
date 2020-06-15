9d7b470d56a5d31a047d05c8e6408744ef5e781e
Phil Jackson <phil@shellarchive.co.uk>
2020-06-15 13:48:59 +0100

    Create an initial CV.

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,6 +1,8 @@
 (ns app.core
   (:require [reagent.dom :as dom]
             [app.router :as router]
+            [app.model.state :as state]
+            [app.model.cv :as cv]
             [app.view.index :as index]
             [app.model.state :as state]
             [app.firebase.auth :as auth]))
@@ -12,7 +14,7 @@
       (not user)
       [index/loader "Loading user..."]
 
-      (not cvs)
+      (not (:selected cvs))
       [index/loader "Loading your CVs..."]
 
       ;; we have all of our data, load the view, giving it a (css)
@@ -29,6 +31,14 @@
 (defn ^:export main []
   (router/init)
   (render)
-  (auth/init #(reset! state/user (if %
-                                   (auth/extract-user %)
-                                   {}))))
+  (auth/init (fn [user]
+               (if user 
+                 ;; we've a valid user so fetch their CV data
+                 (do
+                   (reset! state/user (auth/extract-user user))
+                   (let [new-id (random-uuid)
+                         temp-cv (-> @state/cvs
+                                     (cv/add {:id new-id})
+                                     (cv/select new-id))]
+                     (reset! state/cvs temp-cv)))
+                 (reset! state/user (auth/extract-user user))))))
