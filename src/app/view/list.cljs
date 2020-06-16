221c977c63fdd72030fa1b8c3f9b96ae342b9265
Phil Jackson <phil@shellarchive.co.uk>
2020-06-16 17:50:34 +0100

    Nice looking list.

--- src/app/view/list.cljs
+++ src/app/view/list.cljs
@@ -55,15 +55,14 @@
                              (js->clj (.-dragHandleProps provided)))
        [:div.grab-handle]
        [:div.list-text (line-item-fn ref)]
-       [:div.list-buttons
-        [s/button {:title "Edit"
-                   :size "tiny"
-                   :icon "edit"
-                   :on-click edit-fn}]
-        [s/button {:title "Delete"
-                   :size "tiny"
-                   :icon "delete"
-                   :on-click delete-fn}]]]))])
+       [s/button {:title "Edit"
+                  :size "tiny"
+                  :icon "edit"
+                  :on-click edit-fn}]
+       [s/button {:title "Delete"
+                  :size "tiny"
+                  :icon "delete"
+                  :on-click delete-fn}]]))])
 
 (defn present-list [section new-item-str line-item-fn form]
   (let [updating? (atom false)
