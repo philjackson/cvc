fd01902d8a869d89fe24f46e48baa32aa1403c01
Phil Jackson <phil@shellarchive.co.uk>
2020-06-16 21:17:26 +0100

    Delete fn for CV.

--- src/app/model/cv.cljs
+++ src/app/model/cv.cljs
@@ -1,11 +1,11 @@
 (ns app.model.cv
-  #:ghostwheel.core{:check true :num-tests 10}
+  #:ghostwheel.core{:check true :num-tests 20}
   (:require [app.model.state :as state]
             [cognitect.transit :as transit]
             [app.debug :refer [debug?]]
             [cljs.spec.alpha :as s]
             [ghostwheel.core :as g
-             :refer [>defn => ?]]))
+             :refer [>defn => ? |]]))
 
 (>defn select
   "Select the given CV within `cv-state`."
@@ -24,6 +24,15 @@
   [cv-state]
   [::state/cvs => (? ::state/id)]
   (:selected cv-state))
+
+(>defn delete
+  "Remove a CV and un-select it if need be."
+  [cv-state id]
+  [::state/cvs ::state/id => ::state/cvs | #(not= id (:id (selected cv-state)))]
+  (let [next (update-in cv-state [:docs] dissoc id)]
+    (if (= (:id (selected next)) id)
+      (assoc next :selected nil)
+      next)))
 
 (>defn active-cv-path
   "Gives the path (suitable for `get-in` and co.) to the currently
