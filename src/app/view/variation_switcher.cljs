10f3088ed6e13c6f96fc8a83ae9ac358cc92302b
Phil Jackson <phil@shellarchive.co.uk>
2020-06-17 20:47:46 +0100

    Now possible to inherit a CV.

--- src/app/view/variation_switcher.cljs
+++ src/app/view/variation_switcher.cljs
@@ -35,62 +35,53 @@
          [s/button {:primary true :on-click close} "Done"]]]])))
 
 (defn add-modal [open?]
-  (let [new-name (atom nil)
-        base-on-open? (atom nil)
-        base-on (atom (cv/selected @state/cvs))
-        close (fn []
-                (reset! base-on-open? false)
-                (reset! new-name nil)
-                (reset! open? false))]
-    (fn [open?]
-      [s/modal {:on-close close
-                :open @open?
-                :size "small"
-                :close-icon true}
-       [s/modal-header "Add a new CV"]
-       [s/modal-content
-        [s/form
-         [s/message {:info true
-                     :icon "info"
-                     :header "Why multiple CVs?"
-                     :content (str "Sometimes when you're applying to a particular "
-                                   "company, it may be the case there are skills they "
-                                   "don't want to see that others might. This system allows "
-                                   "you to tailor CVs for specific roles.")}]
-         [s/input-field {:atom new-name :label "CV's name"}]
-         [s/form-group
-          [s/form-field
-           [s/checkbox {:label "Base this CV on one of my others..."
-                        :checked @base-on-open?
-                        :on-change #(reset! base-on-open? (get (js->clj %2) "checked"))
-                        :toggle true}]]]
-         (when @base-on-open?
-           (let [opts (doall
-                       (for [[id cv] (:docs @state/cvs)]
-                         {:key id :text (:name cv) :value id}))]
-             [s/form-field
-              [s/select {:label "Based on:"
-                         :on-change #(reset! base-on (get (js->clj %2) "value"))
-                         :default-value (:value (first opts))
-                         :options opts}]]))]
-        [s/divider]
-        [s/modal-actions
-         [s/button
-          {:type "submit"
-           :primary true
-           :disabled (boolean (not (and @new-name (count @new-name))))
-           :on-click (fn []
-                       (reset! state/cvs
-                               (let [new-uuid (random-uuid)]
-                                 (-> @state/cvs
-                                     (cv/select new-uuid)
-                                     (assoc-in [:docs new-uuid]
-                                               (cond->> {:id new-uuid :name @new-name}
-                                                 @base-on-open?
-                                                 (merge (get-in @state/cvs [:docs @base-on])))))))
-                       (close))}
-          "Add CV"]
-         [s/button {:type "submit" :on-click close} "Cancel"]]]])))
+  (r/with-let [new-name (atom nil)
+               base-on-open? (atom false)
+               close (fn []
+                       (reset! base-on-open? false)
+                       (reset! new-name nil)
+                       (reset! open? false))]
+    [s/modal {:on-close close
+              :open @open?
+              :size "small"
+              :close-icon true}
+     [s/modal-header "Add a new CV"]
+     [s/modal-content
+      [s/form
+       [s/message {:info true
+                   :icon "info"
+                   :header "Why multiple CVs?"
+                   :content (str "Sometimes when you're applying to a particular "
+                                 "company, it may be the case there are skills they "
+                                 "don't want to see that others might. This system allows "
+                                 "you to tailor CVs for specific roles.")}]
+       [s/input-field {:atom new-name :label "CV's name"}]
+       [s/form-group
+        [s/form-field
+         [s/checkbox {:label (str "Base this CV on "
+                                  (get-in @state/cvs
+                                          (cv/active-cv-path @state/cvs :name)))
+                      :checked @base-on-open?
+                      :on-change #(reset! base-on-open? (get (js->clj %2) "checked"))}]]]]
+      [s/divider]
+      [s/modal-actions
+       [s/button
+        {:type "submit"
+         :primary true
+         :disabled (boolean (not (and @new-name (count @new-name))))
+         :on-click (fn []
+                     (let [new-id (random-uuid)
+                           new-cv (if @base-on-open?
+                                    (cv/cv-merge
+                                     (cv/cv-get @state/cvs (cv/selected @state/cvs))
+                                     {:id new-id :name @new-name})
+                                    {:id new-id :name @new-name})]
+                       (reset! state/cvs (-> @state/cvs
+                                             (cv/add new-cv)
+                                             (cv/select new-id))))
+                     (close))}
+        "Add CV"]
+       [s/button {:type "submit" :on-click close} "Cancel"]]]]))
 
 (defn select-variation []
   (let [add-modal-open? (atom false)
