4b69c9cf080a2d64662fde8294d1bc33653123ec
Phil Jackson <phil@shellarchive.co.uk>
2020-06-24 16:22:47 +0100

    Login buttons on menu.

--- src/app/view/menu.cljs
+++ src/app/view/menu.cljs
@@ -13,10 +13,19 @@
        [:div.title "Next CV"]]
       [:div.lower
        [:a {:href "/"} "home"]]]
-     (when user
+     (if (and user (not (empty? user)))
        [:div.right
         [select-variation]
         [s/dropdown {:trigger (r/as-element [:img.user-img {:src (:photo-url user)}])}
          [s/dropdown-menu
           [s/dropdown-item {:text "Logout"
-                            :on-click #(auth/sign-out!)}]]]])]))
+                            :on-click #(auth/sign-out!)}]]]]
+       [:div.login-menu
+        [s/dropdown {:text "Login / signup"}
+         [s/dropdown-menu
+          [s/dropdown-item {:icon "google"
+                            :text "With Google"
+                            :on-click auth/sign-in-google}]
+          [s/dropdown-item {:icon "github"
+                            :text "With Github"
+                            :on-click auth/sign-in-github}]]]])]))
